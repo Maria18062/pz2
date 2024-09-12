70244dcd36409e7eb3d13b10745c48ea1b401a97
User <User@WIN-110HH93FM7Q>
2024-09-12 15:24:43 +0300

    Добавьте файлы проекта.

---
+++ практ2/практ2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main() {
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    // Сума цифр числа
+    int sum = 0;
+    int temp = number;
+    while (temp > 0) {
+        sum += temp % 10;
+        temp /= 10;
+    }
+
+    // Виведення числа у зворотному порядку
+    temp = number;
+    std::cout << "The number is in reverse order: ";
+    while (temp > 0) {
+        std::cout << temp % 10;
+        temp /= 10;
+    }
+    std::cout << std::endl;
+
+    // Результати
+    std::cout << "The sum of the digits of a number: " << sum << std::endl;
+
+    return 0;
+}
